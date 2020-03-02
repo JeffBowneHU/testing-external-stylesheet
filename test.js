8708103c33a81240a6f4de36b1e2548f6bd434a6
JeffBowneHU <46385941+JeffBowneHU@users.noreply.github.com>
2020-03-02 14:40:19 -0500

    add on load

--- test.js
+++ test.js
@@ -1,2 +1,4 @@
-var x = document.getElementsByClassName("byline");
-x[0].innerHTML = "Jarvar Scrips";
+$(window).on("load", function(){
+  var x = document.getElementsByClassName("byline");
+  x[0].innerHTML = "Jarvar Scrips";
+});
